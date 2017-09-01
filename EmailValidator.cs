c4cd73e100be152d89220bb7c83f60f3af3dd93a
Yuriy Loginov <yuriy.loginov@codeproject.com>
2017-09-01 13:51:27 -0400

    initial commit
    first commit

---
+++ EmailValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Utilities
+{
+    /// <summary>
+    /// thanks MSDN: https://msdn.microsoft.com/en-us/library/01escwtf(v=vs.85).aspx
+    /// </summary>
+    public class EmailValidator
+    {
+        private static EmailAddressAttribute emailChecker = new EmailAddressAttribute();
+        private static string[] partsSplitter = new string[] { "@" };
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            //EmailAddressAttribute allows some cases which are illegal
+            //1. consecutive dots
+            //2. '´' character in domain part
+            return emailChecker.IsValid(email) && !email.Contains("..") && AllDomainCharactersLegal(email);
+        }
+
+        private static bool AllDomainCharactersLegal(string email)
+        {
+            string[] parts = email.Split(partsSplitter, System.StringSplitOptions.RemoveEmptyEntries);
+            return parts.Length == 2 && !parts[1].Contains("´");
+        }
+    }    
+}
